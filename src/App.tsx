44027278b7f84004f71aca9f3d0d06044276adc1
jocker-py <buladzislau@gmail.com>
2023-04-26 19:34:28 +0300

    refactor: replace logic of Counter to Controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import Counter from "./components/Counter";
+import {Counter} from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Counter minValue={0} maxValue={5}/>
-      <Counter minValue={0} maxValue={10}/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,32 +1,13 @@
-import React, {FC, useState} from "react";
-import {Screen} from "./Screen/Screen";
-import {Button} from "./Button/Button";
-import s from "./Counter.module.css";
+import React, {} from "react";
+import Controller from "./Controller";
 
-type CounterPropsType = {
-  minValue: number;
-  maxValue: number;
-}
-const Counter: FC<CounterPropsType> = ({minValue, maxValue}) => {
-  const [value, setValue] = useState(minValue);
-  const reset = () => setValue(minValue);
-  const increment = () => setValue(value >= maxValue ? maxValue : value + 1);
-  const decrement = () => setValue(value <= minValue ? minValue : value - 1);
-  return (
-    <div className={s.container}>
-      <Screen value={value} error={value >= maxValue}/>
-      <div className={s.buttons}>
-        <Button callback={decrement}
-                disabled={value <= minValue}
-                bType={"red"}>-</Button>
-        <Button callback={reset}
-                disabled={value <= minValue}>reset</Button>
-        <Button callback={increment}
-                disabled={value >= maxValue}
-                bType={"green"}>+</Button>
-      </div>
-    </div>
-  );
+
+export const Counter = () => {
+  const values = {
+    minValue: 0,
+    maxValue: 1,
+  }
+  return(
+    <Controller {...values}/>
+  )
 };
-
-export default Counter;
