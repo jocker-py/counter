8b413d5b0521043da6b27edeea26f8570b18a074
jocker-py <buladzislau@gmail.com>
2023-04-26 19:39:05 +0300

    feat: add Settings

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,17 @@
 import React, {} from "react";
 import {Controller} from "./Controller/Controller";
-
+import {Settings} from "./Settings/Settings";
 
 
 export const Counter = () => {
   const values = {
     minValue: 0,
     maxValue: 1,
-  }
-  return(
-    <Controller {...values}/>
-  )
+  };
+  return (
+    <>
+      <Controller {...values}/>
+      <Settings/>
+    </>
+  );
 };
