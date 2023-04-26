4c0dcad63138a97d630c5fe31a1913e56130972f
jocker-py <buladzislau@gmail.com>
2023-04-26 19:49:09 +0300

    feat: add SettingsType

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,15 +2,16 @@ import React, {useState} from "react";
 import {Controller} from "./Controller/Controller";
 import {Settings} from "./Settings/Settings";
 
+type SettingsType = {
+  minValue: number
+  maxValue: number
+}
 
 export const Counter = () => {
-  const [settings, setSettings] = useState({
+  const [settings, setSettings] = useState<SettingsType>({
     minValue: 0,
     maxValue: 1,
   });
-  function setMinValue(value: number){
-    setSettings({...settings, minValue: value});
-  }
   return (
     <>
       <Controller {...settings}/>
