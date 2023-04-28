a38b2a923d8fbee79e6dc6bd7e19c5d9365e7bbb
jocker-py <buladzislau@gmail.com>
2023-04-28 19:30:49 +0300

    refactor: refactor code

--- src/components/Controller/Controller.tsx
+++ src/components/Controller/Controller.tsx
@@ -1,29 +1,28 @@
-import React, {FC, useState} from "react";
-import s from "./Counter.module.css";
+import React, {FC, useEffect, useState} from "react";
+import s from "./Controller.module.css";
 import {Button} from "../Button/Button";
 import {Screen} from "../Screen/Screen";
+import {ValuesType} from "../Counter";
 
-type ControllerPropsType = {
-  minValue: number;
-  maxValue: number;
-}
-
+type ControllerPropsType = ValuesType;
 export const Controller: FC<ControllerPropsType> = ({minValue, maxValue}) => {
   const [value, setValue] = useState(minValue);
   const reset = () => setValue(minValue);
-  const increment = () => setValue(value >= maxValue ? maxValue : value + 1);
-  const decrement = () => setValue(value <= minValue ? minValue : value - 1);
+  const increment = () => setValue( value + 1);
+  const decrement = () => setValue(value - 1);
+  const isMinError = value <= minValue;
+  const isMaxError = value >= maxValue;
   return (
     <div className={s.container}>
-      <Screen value={value} error={value >= maxValue}/>
+      <Screen value={value} maxValue={maxValue}/>
       <div className={s.buttons}>
         <Button callback={decrement}
-                disabled={value <= minValue}
+                disabled={isMinError}
                 bType={"red"}>-</Button>
         <Button callback={reset}
-                disabled={value <= minValue}>reset</Button>
+                disabled={isMinError}>reset</Button>
         <Button callback={increment}
-                disabled={value >= maxValue}
+                disabled={isMaxError}
                 bType={"green"}>+</Button>
       </div>
     </div>
